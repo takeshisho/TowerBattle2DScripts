16bb4530b92b4e0ce7a11d0519be12ae5cf69cbf
takeshisho <b2001410@gmail.com>
2024-01-14 18:59:53 +0900

    fix score bug, spawm bug, and  animation bug

--- Main/HitPoint/HitPoint.cs
+++ Main/HitPoint/HitPoint.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 public class HitPoint : MonoBehaviour
 {
     public int Hp;
+    private bool isDead;
 
     protected virtual void Start() {
+        isDead = false;
     }
 
     public virtual void Damage(int damage)
     {
         Hp -= damage;
-        if (Hp <= 0)
+        if(Hp < 0) Hp = 0;
+        if (Hp <= 0 && !isDead)
         {
             Hp = 0;
+            isDead = true;
             OnDie();
         }
     }
--- Main/MobAttack.cs
+++ Main/MobAttack.cs
@@ -43,5 +43,6 @@ public class MobAttack : MonoBehaviour
                 break;
             }            
         }
+        animator.SetBool("isAttack", false);
     }
 }
